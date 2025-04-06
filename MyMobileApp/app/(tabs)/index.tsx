33b2c4573638c347910a6bcd2bdce3fe2536ad9b
MustafaB111213 <mbal111213@gmail.com>
2025-04-07 01:09:46 +0300

    Revert "Add .gitignore file"
    This reverts commit 9fa6cec57a03cb362f313a50da9df3dd0140a3bc.

--- MyMobileApp/app/(tabs)/index.tsx
+++
@@ -1,8 +0,0 @@
-// C:\Users\Mustafa\Github\GitHub\KiralamaPazari\MyMobileApp\app\index.tsx
-import React from 'react';
-// AuthScreen.js dosyanız, proje kökünde olduğu için bir üst dizinden import ediliyor:
-import AuthScreen from '../../AuthScreen';
-
-export default function Index() {
-  return <AuthScreen />;
-}

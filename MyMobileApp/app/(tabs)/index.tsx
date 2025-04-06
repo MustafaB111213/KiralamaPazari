9fa6cec57a03cb362f313a50da9df3dd0140a3bc
MustafaB111213 <mbal111213@gmail.com>
2025-04-07 01:08:23 +0300

    Add .gitignore file

---
+++ MyMobileApp/app/(tabs)/index.tsx
@@ -0,0 +1,8 @@
+// C:\Users\Mustafa\Github\GitHub\KiralamaPazari\MyMobileApp\app\index.tsx
+import React from 'react';
+// AuthScreen.js dosyanız, proje kökünde olduğu için bir üst dizinden import ediliyor:
+import AuthScreen from '../../AuthScreen';
+
+export default function Index() {
+  return <AuthScreen />;
+}
